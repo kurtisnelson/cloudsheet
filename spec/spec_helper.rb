ece67198e8eb13fa1fe48ff462d2946523b00809
Kurt Nelson <kurtisnelson@gmail.com>
2013-06-04 12:06:14 -0400

    Use coveralls for coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require 'simplecov'
-SimpleCov.start
+require 'coveralls'
+Coveralls.wear!
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
